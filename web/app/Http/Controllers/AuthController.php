bbbe3a1fcfac1ff2d9f8c9174099136316558f4b
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-18 12:12:27 +0000

    feat(auth): implement login functionality in AuthController

--- web/app/Http/Controllers/AuthController.php
+++ web/app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;   
 
 class AuthController extends Controller
 {
@@ -22,4 +24,26 @@ class AuthController extends Controller
             'data' => $user
         ], 201); 
     }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = Auth::guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized: Incorrect email or password.',
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User successfully logged in',
+            'data' => [
+                'user' => Auth::user(),
+                'token' => $token
+            ]
+        ], 200);
+    }
+
 }
