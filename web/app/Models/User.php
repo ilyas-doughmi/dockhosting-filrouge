d013f98e48fd917f0795ef8def6b340855ad0c87
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-24 11:29:13 +0100

    feat(config): create dockhosting configuration file

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -57,4 +57,5 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(ActivityLog::class);
     }
+
 }
---
+++ web/config/dockhosting.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'projects_path' => env('DOCKHOSTING_PROJECTS_PATH', storage_path('projects')),
+
+];
