b4f48188eeae2770384666611890e4168b3ddb41
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-24 11:06:34 +0100

    feat(routes): add project route for storing new projects

--- web/routes/api.php
+++ web/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 
 
 Route::prefix('auth')->group(function () {
@@ -13,4 +14,7 @@ Route::prefix('auth')->group(function () {
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
    Route::get('/auth/me', [AuthController::class, 'me']); 
+   
+   // projects
+   Route::post('/projects', [ProjectController::class, 'store']);
 });
