b1dfcf7f71a15f1a12044aba99cf7a6dc08ba28c
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-24 11:06:52 +0100

    feat(project): implement ProjectController with store method for project creation

---
+++ web/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Http\Requests\StoreProjectRequest;
+use App\Jobs\SetupProjectContainer;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends Controller
+{
+    public function store(StoreProjectRequest $request)
+    {
+        $user = Auth::guard('api')->user();
+        $slug = Str::slug($request->name);
+
+        $project = Project::create([
+            'user_id' => $user->id,
+            'technology_id' => $request->technology_id,
+            'name' => $request->name,
+            'slug' => $slug,
+            'directory_path' => "/var/www/dockhosting/projects/{$slug}",
+            'url' => "http://{$slug}.localhost",
+            'status' => 'pending',
+        ]);
+
+        SetupProjectContainer::dispatch($project);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Project creation started! It will be online shortly.',
+            'data' => $project
+        ], 202); 
+    }
+}
