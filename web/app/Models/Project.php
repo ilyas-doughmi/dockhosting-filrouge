b489581a50bff91ac32b0230b14513a955c42930
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-24 11:27:46 +0100

    feat(project): add container relationship to Project model

--- web/app/Models/Project.php
+++ web/app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Project extends Model
 {
@@ -25,5 +26,10 @@ class Project extends Model
     public function technology(): BelongsTo
     {
         return $this->belongsTo(Technology::class);
+    }
+
+    public function container(): HasOne
+    {
+        return $this->hasOne(Container::class);
     }
 }
