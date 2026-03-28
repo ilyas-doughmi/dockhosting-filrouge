ad9b7518a1bd68270a77fc9f87db0734aa5cf4be
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-28 17:58:43 +0100

    feat(routes): add file management routes for project files

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectFileController;
 
 
 Route::prefix('auth')->group(function () {
@@ -13,12 +14,19 @@ Route::prefix('auth')->group(function () {
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
-   Route::get('/auth/me', [AuthController::class, 'me']); 
+    Route::get('/auth/me', [AuthController::class, 'me']); 
    
-   // projects
-   Route::get('/projects', [ProjectController::class, 'index']);
-   Route::post('/projects', [ProjectController::class, 'store']);
-   Route::post('/projects/{project}/stop', [ProjectController::class, 'stop']); 
-   Route::post('/projects/{project}/start', [ProjectController::class, 'start']);
-   Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
+    // projects
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::post('/projects/{project}/stop', [ProjectController::class, 'stop']); 
+    Route::post('/projects/{project}/start', [ProjectController::class, 'start']);
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
+    // file management
+    Route::get('/projects/{project}/files', [ProjectFileController::class, 'index']);
+    Route::get('/projects/{project}/files/read', [ProjectFileController::class, 'show']);
+    Route::post('/projects/{project}/files', [ProjectFileController::class, 'store']);
+    Route::put('/projects/{project}/files', [ProjectFileController::class, 'update']);
+    Route::delete('/projects/{project}/files', [ProjectFileController::class, 'destroy']);
+    Route::post('/projects/{project}/files/upload', [ProjectFileController::class, 'upload']);
 });
